d81c8f7a0c9a1e4661722d9bd51b127899570f4e
marc.dacanay <marcdacz@users.noreply.github.com>
2017-09-30 21:32:46 +1000

    removing obsolete firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAkIPVzQTDc09Oxj6inLhN0TWJizUQrvuk",
-    authDomain: "angular4-online-shop.firebaseapp.com",
-    databaseURL: "https://angular4-online-shop.firebaseio.com",
-    projectId: "angular4-online-shop",
-    storageBucket: "angular4-online-shop.appspot.com",
-    messagingSenderId: "871263500312"
+    // Copy Firebase Configs Here
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAkIPVzQTDc09Oxj6inLhN0TWJizUQrvuk",
-    authDomain: "angular4-online-shop.firebaseapp.com",
-    databaseURL: "https://angular4-online-shop.firebaseio.com",
-    projectId: "angular4-online-shop",
-    storageBucket: "angular4-online-shop.appspot.com",
-    messagingSenderId: "871263500312"
+    // Copy Firebase Configs Here
   }
 };
